656353db761d2aca304c488c77b78816e585ba13
william-wwh <william-wwh@github.com>
2020-04-08 11:42:31 +0800

    4.8

--- class4/Text3.java
+++ class4/Text3.java
@@ -4,6 +4,11 @@ public class Text3 {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
+		/* 小明参加马拉松长跑比赛，绕400米操场跑50圈，前10圈最有力气的
+		 * 时候，以5m/s 的速度在匀速奔跑，10圈之后力气不够了，以每2圈都
+		 * 降0.1m/s的速度匀速奔跑为最后冲刺蓄力，到了最后2圈，以全力最
+		 * 快速度8m/s的速度冲刺。请问，小明跑完这次马拉松用时多久？*/
+		 
 		int i = 1;
 		double s = 0;
 		double time = 0;
