9095a2ff63c17bb413b4a5d0e53a14bd366b2509
william-wwh <william-wwh@github.com>
2020-04-25 21:08:17 +0800

    4.25

--- class16/Text1.java
+++ class16/Text1.java
@@ -6,46 +6,43 @@ public class Text1 {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
+		int a,b;
+		Scanner sr = new Scanner(System.in);
 		System.out.println("请输入正整数");
 		System.out.println("输入0退出");
 		do {
-			int a,b,d=0;
-			Scanner sr = new Scanner(System.in);
-			System.out.print("输入第一个正整数: ");
-			a= sr.nextInt();
-			if(a==0) {
-				System.out.println("你输入了0,程序退出");
-				break;
-			}else if(a<0) {
-				System.out.println("你输入了负数,请重新输入");
+			System.out.print("请输入第一个数: ");
+			a = sr.nextInt();
+			if(a<0) {
 				continue;
+			}else if(a == 0) {
+				System.out.println("你输入了0,程序已退出");
+				break;
 			}
-			System.out.print("输入第二个正整数: ");
-			b= sr.nextInt();
-			if(b==0) {
-				System.out.println("你输入了0,程序退出");
-				break;
-			}else if(b<0) {
-				System.out.println("你输入了负数,请重新输入");
+			System.out.print("请输入第二个数: ");
+			b = sr.nextInt();
+			if(b<0) {
 				continue;
+			}else if(b == 0){
+				System.out.println("你输入了0,程序已退出");
+				break;
 			}
 			
-			/***最大公约数***/
-			if(a<b) {
-				int c = 0;
-				c=a;
-				a=b;
-				b=c;
-			}
-			int c = 0;
 			int m,n;
 			m=a;
 			n=b;
-			do{
-				d=m%n;
-				c=m/n;
+			if(m<n) {
+				int c=0;
+				c=m;
+				m=n;
+				n=c;
+			}
+			int d = 0,c = 0;
+			do {
+				d = m%n;
+				c = m/n;
 				if(d==0) {
-					System.out.println("最大公约数是"+c);
+					System.out.println("最大公约数是"+n);
 					break;
 				}else {
 					m=n;
@@ -53,13 +50,13 @@ public class Text1 {
 				}
 			}while(true);
 			
-			/***最小公倍数***/
-			int x,y;
+			
+			int x, y,j ;
 			x=a/c;
 			y=b/c;
-			int j =x*y*c;
+			j= x*y*c;
 			System.out.println("最小公倍数是"+j);
-		}while(true);
+		} while (true);
 	}
-		
+
 }
