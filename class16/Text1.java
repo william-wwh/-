27a1f17f5b4e17f5bb4b8e2ca838df7938fa1cee
william-wwh <william-wwh@github.com>
2020-04-27 13:15:10 +0800

    4/27

--- class16/Text1.java
+++ class16/Text1.java
@@ -9,56 +9,58 @@ public class Text1 {
 		System.out.println("请输入正整数");
 		System.out.println("输入0退出");
 		do {
-			int a,b,d=0;
+			int a,b;
 			Scanner sr = new Scanner(System.in);
 			System.out.print("输入第一个正整数: ");
 			a= sr.nextInt();
-			if(a==0) {
+			if(0 == a) {
 				System.out.println("你输入了0,程序退出");
 				break;
-			}else if(a<0) {
+			}else if(a < 0) {
 				System.out.println("你输入了负数,请重新输入");
 				continue;
 			}
 			System.out.print("输入第二个正整数: ");
 			b= sr.nextInt();
-			if(b==0) {
+			if(0 == b) {
 				System.out.println("你输入了0,程序退出");
 				break;
-			}else if(b<0) {
+			}else if(b < 0) {
 				System.out.println("你输入了负数,请重新输入");
 				continue;
 			}
 			
 			/***最大公约数***/
-			if(a<b) {
+			if(a < b) {
 				int c = 0;
-				c=a;
-				a=b;
-				b=c;
+				c = a;
+				a = b;
+				b = c;
 			}
-			int c = 0;
-			int m,n;
-			m=a;
-			n=b;
+            int quotient = 0;   //商
+            int remainder = 0;  //余数
+			int dividend,divisor;   //被除数,除数
+			dividend = a;
+			divisor = b;
 			do{
-				d=m%n;
-				c=m/n;
-				if(d==0) {
-					System.out.println("最大公约数是"+n);
+				remainder = dividend % divisor; //求余数
+                quotient = dividend / divisor;  //求商
+				if(remainder == 0) {
+					System.out.println("最大公约数是"+ divisor);
 					break;
 				}else {
-					m=n;
-					n=d;
+					dividend = divisor;
+					divisor = remainder;
 				}
 			}while(true);
 			
 			/***最小公倍数***/
-			int x,y;
-			x=a/n;
-			y=b/n;
-			int j =x*y*c;
-			System.out.println("最小公倍数是"+j);
+			int remainder1,remainder2;
+			remainder1 = a / divisor;
+			remainder2 = b / divisor;
+			int leastCommonMultiple =remainder1 * remainder2 * divisor;
+			System.out.println("最小公倍数是"+ leastCommonMultiple);
+			System.out.println();
 		}while(true);
 	}
 		
